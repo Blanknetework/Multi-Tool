1e5725ab498ccc59c03740f95d5fedf343f000af
Adam Nuevo <adamnuevo66@gmail.com>
2024-12-31 04:43:09 -0800

    Fixing some bugs

--- src/multi_tool.py
+++ src/multi_tool.py
@@ -3,30 +3,47 @@ import ip_lookup
 import webhook_sender
 import password_generator
 import wifi_brute_force
+import shutil
 from colorama import init, Fore, Style
 
 # Initialize colorama
 init(autoreset=True)
 
 def print_banner():
-    banner = r"""
-+----------------------------------------------------------------------------+
-|                                                                            |
-|  __  __ _    _ _   _______ _____            _______ ____   ____  _         |
-| |  \/  | |  | | | |__   __|_   _|          |__   __/ __ \ / __ \| |        |
-| | \  / | |  | | |    | |    | |    ______     | | | |  | | |  | | |        |
-| | |\/| | |  | | |    | |    | |   |______|    | | | |  | | |  | | |        |
-| | |  | | |__| | |____| |   _| |_              | | | |__| | |__| | |____    |
-| |_|  |_|\____/|______|_|  |_____|             |_|  \____/ \____/|______|   |
-|                                                                            |
-|                            MULTI-TOOL                                      |
-|                                                                            |
-|                            Created by: Jigsaww                             |
-|                                                                            |
-+----------------------------------------------------------------------------+
-    """
-    print(Fore.CYAN + banner)
+    terminal_width = shutil.get_terminal_size().columns
+    banner_text = [
+        r" __  __ _    _ _   _______ _____            _______ ____   ____  _      ",
+        r"|  \/  | |  | | | |__   __|_   _|          |__   __/ __ \ / __ \| |     ",
+        r"| \  / | |  | | |    | |    | |    ______     | | | |  | | |  | | |     ",
+        r"| |\/| | |  | | |    | |    | |   |______|    | | | |  | | |  | | |     ",
+        r"| |  | | |__| | |____| |   _| |_              | | | |__| | |__| | |____ ",
+        r"|_|  |_|\____/|______|_|  |_____|             |_|  \____/ \____/|______|",
+    ]
+    created_by_text = "Created by: Jigsaww"
+    
+    # Ensure banner width is between 40 and terminal width
+    banner_width = max(40, min(terminal_width - 4, 120))
+    border = "+" + "-" * (banner_width - 2) + "+"
+    
+    
+   # Center the text within the banner
+    banner_lines = [border]
+    for line in banner_text:
+        if len(line) > banner_width - 2:
+            # Truncate the line if it's too long
+            line = line[:banner_width - 2]
+        banner_lines.append("|" + line.center(banner_width - 2) + "|")
+    banner_lines.append("|" + " " * (banner_width - 2) + "|")
+    banner_lines.append("|" + created_by_text.center(banner_width - 2) + "|")
+    banner_lines.append("|" + " " * (banner_width - 2) + "|")
+    banner_lines.append(border)
+    
+    
+    # Print the banner
+    for line in banner_lines:
+        print(Fore.CYAN + line)
     print(Style.RESET_ALL)
+
 
 def main_menu():
     while True:
